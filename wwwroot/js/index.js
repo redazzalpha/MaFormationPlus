f9d367b6fef4486478c0d5283109b7f2d741ceb1
redazzkillah <redazzkillah@gmail.com>
2022-08-05 22:53:18 +0200

    relook home page on carrers & modules

--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -77,3 +77,94 @@ chevronRight.addEventListener('mouseover', (e) => {
 chevronRight.addEventListener('mouseout', (e) => {
     chevronRight.classList.remove('chevron-hover');
 });
+
+//const slider1 = document.querySelector('.items1');
+//let scrollTop;
+//let mousePos;
+//let offsetTop;
+//let offset;
+
+//test.addEventListener('mousedown', (e) => {
+//    isDown = true;
+//    slider1.classList.add('active');
+
+
+
+
+
+
+
+//    startX = e.pageY - slider1.scrollTop;
+//    scrollTop = slider1.scrollHeight;
+
+
+
+//    position = test.clientHeight + test.scrollTop;
+//    end = test.scrollHeight;
+
+//    console.log("-------------------------------------------\n")
+//    console.log("test.clientHeight: " + test.clientHeight);
+//    console.log("test.scrollTop: " + test.scrollTop);
+//    console.log("test.scrollHeight: " + test.scrollHeight);
+//    console.log("-------------------------------------------\n")
+
+//    console.log("e.pageY: " + e.pageY + " test.offsetTop: " + test.offsetTop, "test.scrollTop: " + test.scrollTop, "test.clientHeight: " + test.clientHeight, "test.scrollHeight: " + test.scrollHeight);
+
+
+
+//    //e.pageY
+//    //test.offsetTop
+//    //test.scrollTop
+//    //test.clientHeight
+//    //test.scrollHeight
+
+//    mousePos = e.pageY;
+//    mousePos = mousePos - (test.offsetTop + 21);
+//    offset = test.clientHeight - mousePos;
+//    mousePos = mousePos + offset;
+
+
+//    var v = test.scrollTop + mousePos;
+
+//    startX = e.pageY;
+
+//    //console.log(v + " : " + test.scrollHeight);
+
+//});
+//test.addEventListener('mouseup', () => {
+//    isDown = false;
+//    slider1.classList.remove('active');
+//});
+//test.addEventListener('mousemove', (e) => {
+//    if (!isDown) return;
+//    e.preventDefault();
+//    const x = e.pageY - slider1.scrollTop;
+//    let walk = (x - startX) * 3;
+
+
+
+
+
+
+//    mousePos = e.pageY;
+//    offsetTop = test.offsetTop;
+//    mousePos = mousePos - (offsetTop + 21);
+//    offset = test.clientHeight - mousePos;
+//    mousePos = mousePos + offset;
+
+//    walk = test.scrollTop + mousePos;
+
+//    //test.scroll(0, startX - walk);
+//    console.log("startX: " + startX + " e.pageY: " + e.pageY + " difference: " + (e.pageY - startX));
+//    test.scrollTop = test.scrollTop + (e.pageY - startX) / 10;
+
+
+
+
+
+//});
+//test.addEventListener('mouseleave', () => {
+//    isDown = false;
+//    slider1.classList.remove('active');
+//});
+
